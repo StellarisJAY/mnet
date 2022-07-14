96bd4b6a84891fc8cee5985c1a3933752fce22df
StellarisJAY <760409558@qq.com>
2022-07-14 14:37:13 +0800

    feat: 添加TCP的Server实现

---
+++ server/tcp/server.go
@@ -0,0 +1,129 @@
+package tcp
+
+import (
+	"context"
+	"fmt"
+	"github.com/StellarisJAY/mnet/interface/network"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"strconv"
+	"sync"
+	"sync/atomic"
+	"syscall"
+)
+
+type Server struct {
+	port  int
+	conns sync.Map
+	proto network.Protocol
+
+	nextConnId uint32
+	ctx        context.Context
+	cancel     context.CancelFunc
+
+	listener net.Listener
+
+	startHook network.ServerStartHook
+	closeHook network.ServerCloseHook
+
+	closeChan chan interface{}
+}
+
+func MakeTcpServer(port int, protocol network.Protocol) *Server {
+	s := new(Server)
+	s.port = port
+	s.proto = protocol
+	s.conns = sync.Map{}
+	s.nextConnId = 0
+	s.ctx, s.cancel = context.WithCancel(context.Background())
+	s.closeChan = make(chan interface{})
+	return s
+}
+
+func (s *Server) Start() (error, chan interface{}) {
+	listener, err := net.Listen("tcp", ":"+strconv.Itoa(s.port))
+	if err != nil {
+		return fmt.Errorf("start server error: %v", err), nil
+	}
+	s.listener = listener
+
+	// listen to os signals
+	signals := make(chan os.Signal)
+	signal.Notify(signals, syscall.SIGHUP, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT)
+	go func() {
+		sig := <-signals
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGKILL, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT:
+			// use context.CancelFunc to stop server
+			s.cancel()
+		}
+	}()
+
+	go func() {
+		for {
+			conn, acceptErr := s.listener.Accept()
+			if acceptErr != nil {
+				break
+			}
+			// accepted connection, generate conn ID and bind protocol
+			connection := MakeTcpConnection(conn, atomic.AddUint32(&s.nextConnId, 1), s.proto)
+			s.conns.Store(connection, true)
+			// start connection's IO loop
+			go connection.Start()
+		}
+	}()
+	// call start hook after starting server
+	if s.startHook != nil {
+		s.startHook()
+	}
+	log.Println("TCP server started, listening: ", s.port)
+
+	go func() {
+		// wait for close signal
+		select {
+		case <-s.ctx.Done():
+			s.gracefulClose()
+		}
+	}()
+	return nil, s.closeChan
+}
+
+// Shutdown server gracefully
+func (s *Server) gracefulClose() {
+	log.Println("shutting down tcp server...")
+	// call shutdown hook
+	if s.closeHook != nil {
+		s.closeHook()
+	}
+	// close all client connections
+	s.conns.Range(func(key, value interface{}) bool {
+		connection := key.(*Connection)
+		connection.Close()
+		return true
+	})
+	// close listener
+	_ = s.listener.Close()
+	s.closeChan <- 1
+}
+
+func (s *Server) Close() {
+	s.cancel()
+}
+
+func (s *Server) Protocol() network.Protocol {
+	return s.proto
+}
+
+func (s *Server) SetProtocol(p network.Protocol) {
+	s.proto = p
+}
+
+func (s *Server) SetServerStartHook(hook network.ServerStartHook) {
+	s.startHook = hook
+}
+
+func (s *Server) SetServerCloseHook(hook network.ServerCloseHook) {
+	s.closeHook = hook
+}
