4d9ccd7bdc52d63063dbeba2266e4f679a436eca
StellarisJAY <760409558@qq.com>
2022-07-14 14:36:36 +0800

    refactor: 修改Protocol的Decode和Encode方法声明；新增HandleWithWorker方法

--- interface/network/protocol.go
+++ interface/network/protocol.go
@@ -2,15 +2,13 @@ package network
 
 import "io"
 
-type DecodeFunc func(reader io.Reader) Packet
-type EncodeFunc func(packet Packet) []byte
-
 type Protocol interface {
-	Decoder() DecodeFunc // Protocol's Decoder
-	Encoder() EncodeFunc // Protocol's Encoder
+	Decode(reader io.Reader) (Packet, error) // Protocol's Decoder
+	Encode(packet Packet) ([]byte, error)    // Protocol's Encoder
 
 	HeaderLength() uint32 // Protocol's HeaderLength in bytes
-	Code() uint32         // Protocol's unique ID Code
+	Code() byte           // Protocol's unique ID Code
 
-	Handle(conn Connection, packet Packet) // HandleFunc of this protocol
+	Handle(conn Connection, packet Packet)           // HandleFunc of this protocol
+	HandleWithWorker(conn Connection, packet Packet) // use protocol's worker pool to handle
 }
