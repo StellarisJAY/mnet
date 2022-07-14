77dccf8da7bd5d9c6cb851b0e3498f3babb63c3d
StellarisJAY <760409558@qq.com>
2022-07-14 16:40:54 +0800

    refactor: 添加Client接口的注释

--- interface/network/client.go
+++ interface/network/client.go
@@ -1,8 +1,12 @@
 package network
 
 type Client interface {
+	// Oneway send a packet without response
 	Oneway(address string, packet Packet) error
+	// Request send a packet and wait for response
 	Request(address string, packet Packet) (response Packet, err error)
+	// Future send a packet and get a channel to receive response
 	Future(address string, packet Packet) (chan Packet, error)
+	// Async send a packet and do nothing, client will call callback automatically when receiving response
 	Async(address string, packet Packet, callback func(packet Packet, err error))
 }
